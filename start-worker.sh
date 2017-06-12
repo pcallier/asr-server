0681b779d97b746aa16fb4f07de3bc0c5ac08e91
Patrick Callier <pcallier@iqt.org>
2017-06-12 15:58:43 -0700

    Print worker start command

--- start-worker.sh
+++ start-worker.sh
@@ -33,4 +33,5 @@ fi;
 #start worker and connect it to the master
 export GST_PLUGIN_PATH=/opt/gst-kaldi-nnet2-online/src/:/opt/kaldi/src/gst-plugin/
 
+echo "Worker command: /opt/kaldi-gstreamer-server/kaldigstserver/worker.py -c $YAML -u ws://$MASTER:$PORT/worker/ws/speech"
 python /opt/kaldi-gstreamer-server/kaldigstserver/worker.py -c $YAML -u ws://$MASTER:$PORT/worker/ws/speech
